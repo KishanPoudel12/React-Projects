223336440f6659b141447df98e8d3a61246aa328
kishan <kishanpoudel563@gmail.com>
2025-01-20 13:38:08 +0530

    deleted a copy of previous Folder

--- Food_Recipe_App/vite.config.js
+++ Food_Recipe_App/vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // Replace 'your-repo-name' with your actual repository name
 export default defineConfig({
   base: "/React-Projects/Food_Recipe_App/", // For GitHub Pages deployment
-
   plugins: [react()],
   server: {
     port: 3000, // Optional: Change the local dev server port
